47e32cfe3b83ddafd7520dc7d23cc4f6457a0496
Recognizer83 <121746524+Recognizer83@users.noreply.github.com>
2025-03-15 19:24:06 +0600

    Update wezterm.lua

--- arkanelinux-generic/overlay/post_install/etc/wezterm/wezterm.lua
+++ arkanelinux-generic/overlay/post_install/etc/wezterm/wezterm.lua
@@ -3,10 +3,10 @@ local config = {}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = '30pt',
-	right = '30pt',
-	top = '24pt',
-	bottom = '24pt',
+	left = '24pt',
+	right = '24pt',
+	top = '18pt',
+	bottom = '18pt',
 }
 config.use_fancy_tab_bar = false
 
